22dce5ad32a6914ddd8c9210b50a5148d930ba5f
tssj-proj <tssj.proj@gmail.com>
2025-05-08 14:26:37 +0900

    Initial Commit

---
+++ src/main/java/me/tssj/tssj_cord/TssjCordApplication.java
@@ -0,0 +1,13 @@
+package me.tssj.tssj_cord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TssjCordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TssjCordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/tssj/tssj_cord/TssjCordApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tssj.tssj_cord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TssjCordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
